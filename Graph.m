4d38be7f1ebcc51028ea03d88e09208cd4fdbb88
ZacFhy <137105375+ZacFhy@users.noreply.github.com>
2023-07-08 04:09:23 +0100

    .

--- Graph.m
+++ Graph.m
@@ -1,8 +1,8 @@
 clear
 
-
 %% 
-addpath('\\icnas3.cc.ic.ac.uk\zyf22\Desktop\32\gramm-master')
+% addpath('\\icnas3.cc.ic.ac.uk\zyf22\Desktop\32\gramm-master')
+addpath('D:\Github\MSc\gramm-master\gramm')
 
 %%
 fn='03_SAA8003 data.xlsx';        % using a fully-qualified filename here would be good practice
@@ -17,6 +17,13 @@ for i=1:length(shts)
   data{i}(1:3,:) = []
   date{i}=date{i}(2,:)
 end
-%% 
+%% testing
+ha = data{1}(:,2)
+a = data{1}(:,1)
+ g=gramm('x',ha,'y',a);
+% ,'color',date{1}(2),'subset','subset',cars.Cylinders~=3 & cars.Cylinders~=5
+g.set_names('x','shit','y','on9')
+g.geom_line();
+g.draw()
+%% test
 
-g=gramm('x',cars.Model_Year,'y',cars.MPG,'color',cars.Cylinders,'subset',cars.Cylinders~=3 & cars.Cylinders~=5);
