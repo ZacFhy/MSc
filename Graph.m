987bcf4ce18d00c89223f3aff05903d8ac4b6d1f
ZacFhy <137105375+ZacFhy@users.noreply.github.com>
2023-07-08 18:44:54 +0100

    .

--- Graph.m
+++ Graph.m
@@ -2,28 +2,39 @@ clear
 
 %% 
 % addpath('\\icnas3.cc.ic.ac.uk\zyf22\Desktop\32\gramm-master')
-addpath('D:\Github\MSc\gramm-master\gramm')
+addpath('D:\Github\MSc\gramm-master')
 
 %%
 fn='03_SAA8003 data.xlsx';        % using a fully-qualified filename here would be good practice
-opt=detectImportOptions(fn);
+opt = detectImportOptions(fn);
 % wanted/need options code/fixup here...
 shts=sheetnames(fn);
 data={}
 date = {}
 for i=1:length(shts)
-  data{i} = readtable(fn,opt,'sheet',shts(i))
+  data{i} = readmatrix(fn,opt,'sheet',shts(i))
   date{i} = readcell(fn,opt,'sheet',shts(i))
   data{i}(1:3,:) = []
-  date{i}=date{i}(2,:)
+  date{i}=date{i}(1,:)
+  
 end
+
+% for i=1:length(data{1}(:,1))
+%     for j=1:length(data{1}(1,:))
+% date{1}(i,j)=date{1}(1,j)
+%     end
+% end
 %% testing
-ha = data{1}(:,2)
+ha = data{1}(:,2:24)
 a = data{1}(:,1)
- g=gramm('x',ha,'y',a);
-% ,'color',date{1}(2),'subset','subset',cars.Cylinders~=3 & cars.Cylinders~=5
-g.set_names('x','shit','y','on9')
-g.geom_line();
-g.draw()
+c = date{1}(:,2:24)
+plot(ha,a)
+
+% for i=1:length(date{1}(2:24))
+%   g{i}=sprintf('%d',date{1}(i));
+% end
+legend(c)
+
+ % LegendsStrings{i} = ['variance_a = ',num2str(variance_a(i))];
 %% test
 
